b66df6f5689aa67a2b2f8e761854aca54fd25d25
Andrew Molchanov <mr.catmanrus@gmail.com>
2020-10-09 05:18:33 +0300

    Фикс ошибки вызываемой при открытии DevTools

--- src/main/index.ts
+++ src/main/index.ts
@@ -21,9 +21,11 @@ function createMainWindow() {
     }
   })
 
-  if (isDevelopment) {
-    // window.webContents.openDevTools()
-  }
+  window.webContents.on('did-frame-finish-load', () => {
+    if (isDevelopment) {
+      window.webContents.openDevTools()
+    }
+  })
 
   if (isDevelopment) {
     window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`)
