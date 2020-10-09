34f4cf44f531501af2f32f70e25d0fd25517c1f6
Andrew Molchanov <mr.catmanrus@gmail.com>
2020-10-09 04:07:00 +0300

    Интеграция Webpack + TypeScript + Vue.js + SASS

---
+++ src/renderer/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Aurora Launcher Test</h1>
+</template>
+
+
+<style lang="scss">
+body {
+    background-image: url('./assets/images/background.png');
+    color: #fff;
+}
+</style>
