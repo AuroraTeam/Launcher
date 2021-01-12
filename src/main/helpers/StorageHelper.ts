501ec0410c3e41ad11afa9536236bcc33b0df71e
Andrew Molchanov <mr.catmanrus@gmail.com>
2021-01-12 20:45:52 +0300

    StorageHelper

---
+++ src/main/helpers/StorageHelper.ts
@@ -0,0 +1,19 @@
+import * as fs from "fs"
+import * as path from "path"
+
+export class StorageHelper {
+    // static storageDir: string = (process.env.DEV || false) ? __dirname : path.resolve(__dirname, '../game') 
+    static storageDir: string = __dirname
+    static updatesDir: string = path.resolve(StorageHelper.storageDir, "assets")
+    static clientsDir: string = path.resolve(StorageHelper.storageDir, "clients")
+    static logsDir: string = path.resolve(StorageHelper.storageDir, "logs")
+    static tempDir: string = path.resolve(StorageHelper.storageDir, "temp")
+    static logFile: string = path.resolve(StorageHelper.logsDir, "Launcher.log")
+
+    static createMissing(): void {
+        if (!fs.existsSync(this.updatesDir)) fs.mkdirSync(this.updatesDir)
+        if (!fs.existsSync(this.clientsDir)) fs.mkdirSync(this.clientsDir)
+        if (!fs.existsSync(this.logsDir)) fs.mkdirSync(this.logsDir)
+        if (!fs.existsSync(this.tempDir)) fs.mkdirSync(this.tempDir)
+    }
+}
