9b35b84a46506426e70f21be26ee5a7ccd6cc0de
Andrew Molchanov <mr.catmanrus@gmail.com>
2021-11-29 00:50:26 +0300

    Вынес с отдельный файл typings внутреннего api

--- src/renderer/vue-shims.d.ts
+++ src/renderer/vue-shims.d.ts
@@ -4,9 +4,3 @@ declare module '*.vue' {
     import Vue from 'vue';
     export default Vue;
 }
-
-declare type LauncherAPI = typeof import('../preload/index').api;
-declare interface Window {
-    launcherAPI: LauncherAPI;
-}
-declare const launcherAPI: LauncherAPI;
