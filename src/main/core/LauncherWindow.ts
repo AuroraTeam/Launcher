e4d5b4a32b54138ada32aac70b036b8bdeea417d
Andrew Molchanov <mr.catmanrus@gmail.com>
2023-10-28 00:40:04 +0300

    init electron-forge

--- src/main/core/LauncherWindow.ts
+++ src/main/core/LauncherWindow.ts
@@ -20,6 +20,7 @@ export class LauncherWindow {
      * Launcher initialization
      */
     createWindow() {
+        if (require('electron-squirrel-startup')) app.quit();
         // This method will be called when Electron has finished
         // initialization and is ready to create browser windows.
         // Some APIs can only be used after this event occurs.
