8c40fd6979a88955d908bb3589fc17fed6f57218
Andrew Molchanov <mr.catmanrus@gmail.com>
2023-10-31 06:48:36 +0300

    fix

--- forge.config.js
+++ forge.config.js
@@ -1,31 +1,37 @@
 module.exports = {
-  packagerConfig: {
-    asar: true,
-    dir: "build"
-  },
-  rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
+    packagerConfig: {
+        asar: true,
+        afterCopy: [
+            //TODO Remove trash
+            (buildPath, electronVersion, platform, arch, callback) => {
+                console.log(buildPath);
+                callback();
+            },
+        ],
     },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-  ],
+    rebuildConfig: {},
+    makers: [
+        {
+            name: '@electron-forge/maker-squirrel',
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-zip',
+            platforms: ['darwin'],
+        },
+        {
+            name: '@electron-forge/maker-deb',
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-rpm',
+            config: {},
+        },
+    ],
+    plugins: [
+        {
+            name: '@electron-forge/plugin-auto-unpack-natives',
+            config: {},
+        },
+    ],
 };
