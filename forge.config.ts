caa287deb0f88526f065ab139845c6088468b399
Andrew Molchanov <mr.catmanrus@gmail.com>
2023-11-04 04:49:48 +0300

    удаляем ненужные файлы при сборке

--- forge.config.ts
+++ forge.config.ts
@@ -1,13 +1,17 @@
-module.exports = {
+import { ForgeConfig } from '@electron-forge/shared-types';
+
+const config: ForgeConfig = {
     packagerConfig: {
         asar: true,
-        afterCopy: [
-            //TODO Remove trash
-            (buildPath, electronVersion, platform, arch, callback) => {
-                console.log(buildPath);
-                callback();
-            },
-        ],
+        ignore(path) {
+            if (!path) return false;
+            if (path.startsWith('/package.json')) return false;
+            if (path.startsWith('/build')) return false;
+            if (path === '/node_modules') return false;
+            if (path.startsWith('/node_modules/electron-squirrel-startup'))
+                return false;
+            return true;
+        },
     },
     rebuildConfig: {},
     makers: [
@@ -18,6 +22,7 @@ module.exports = {
         {
             name: '@electron-forge/maker-zip',
             platforms: ['darwin'],
+            config: {},
         },
         {
             name: '@electron-forge/maker-deb',
@@ -35,3 +40,5 @@ module.exports = {
         },
     ],
 };
+
+export default config;
