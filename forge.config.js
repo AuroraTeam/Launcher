5ec07b8fcde412c7dbd64b10c60bfa007cea7103
Andrew Molchanov <mr.catmanrus@gmail.com>
2023-11-11 04:39:48 +0300

    Revert "check build"
    This reverts commit bf159ae3aad5f55ec43531ba2d2a4cb0437d9bbb.

---
+++ forge.config.js
@@ -0,0 +1,43 @@
+/**
+ * @type {import('@electron-forge/shared-types').ForgeConfig}
+ */
+module.exports = {
+    packagerConfig: {
+        asar: true,
+        ignore(path) {
+            if (!path) return false;
+            if (path.startsWith('/package.json')) return false;
+            if (path.startsWith('/build')) return false;
+            if (path === '/node_modules') return false;
+            if (path.startsWith('/node_modules/electron-squirrel-startup'))
+                return false;
+            return true;
+        },
+    },
+    rebuildConfig: {},
+    makers: [
+        {
+            name: '@electron-forge/maker-squirrel',
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-zip',
+            platforms: ['darwin'],
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-deb',
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-rpm',
+            config: {},
+        },
+    ],
+    plugins: [
+        {
+            name: '@electron-forge/plugin-auto-unpack-natives',
+            config: {},
+        },
+    ],
+};
