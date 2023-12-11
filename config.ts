6f40a09d7b02b8ac5bf4316c2da3bd03f85e9014
Andrew Molchanov <mr.catmanrus@gmail.com>
2023-12-12 02:15:09 +0300

    small edits

--- config.ts
+++ config.ts
@@ -11,7 +11,7 @@ export const window = {
 };
 
 export const api = {
-    ws: 'ws://212.109.221.239:1370/ws',
-    web: 'http://212.109.221.239:1370',
+    ws: 'ws://127.0.0.1:1370/ws',
+    web: 'http://127.0.0.1:1370',
     // extraToken: token,
 };
--- src/renderer/runtime/components/TitleBar/states.ts
+++ src/renderer/runtime/components/TitleBar/states.ts
@@ -11,7 +11,7 @@ export const titlebarTitle = atom({
     key: 'titlebar.title',
     default: {
         show: true,
-        text: 'AuroraLauncher v0.0.4-beta',
+        text: 'AuroraLauncher v0.0.4-rc.2',
     },
 });
 
