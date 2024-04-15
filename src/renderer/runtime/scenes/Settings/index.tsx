f59cfd71e749278196c96511f4f270cb3db83355
kostya-main <simkin-kostya@mail.ru>
2024-04-15 03:44:17 +0300

    Рабочие кнопки выбора страницы и вывод данных со слайдера

--- src/renderer/runtime/scenes/Settings/index.tsx
+++ src/renderer/runtime/scenes/Settings/index.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState, MutableRefObject } from 'react';
+import If from '../../components/If';
 
 import { useTitlebar } from '../../components/TitleBar/hooks';
 import classes from './index.module.sass';
@@ -12,45 +13,74 @@ export default function ServersList() {
         setTitlebarTitleText('Настройки лаунчера');
     }, []);
 
-    const main = true
+    const [main, EditeButtonMain] = useState(true);
+    const [info, EditeButtonInfo] = useState(false);
+
+    const RAMvalue = useRef() as MutableRefObject<HTMLLabelElement>;
+
+    const Slader = (e:React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value
+        RAMvalue.current.innerText = `Выделено оперативной памяти: ${value}`
+    }
+
+    const Button = (type: string) => {
+        switch (type) {
+            case 'main':
+                EditeButtonMain(true);
+                EditeButtonInfo(false);
+                return
+            case 'info':
+                EditeButtonMain(false);
+                EditeButtonInfo(true);
+                return
+        }
+    };
 
     return (
         <div className={classes.window}>
             <div className={classes.buttonsList}>
                 <div className={classes.buttons}>
-                    <button disabled = {main}>
+                    <button onClick={() => Button('main')} disabled = {main}>
                         Основное
                     </button>
-                    <button >
+                    <button onClick={() => Button('info')} disabled = {info}>
                         О лаунчере
                     </button>
                 </div>
             </div>
-            <div className={classes.options}>
-                <label className={classes.checkbox}>
-                    <input type="checkbox"  />
-                    <span className={classes.checkboxSwitch}></span>
-                    Test
-                </label>
-                <label className={classes.checkbox}>
-                    <input type="checkbox"  />
-                    <span className={classes.checkboxSwitch}></span>
-                    Test2
-                </label>
-                <label className={classes.checkbox}>
-                    <input type="checkbox"  />
-                    <span className={classes.checkboxSwitch}></span>
-                    Test3
-                </label>
-                <input className={classes.slider} type="range" list="values" />
-                <datalist id="values">
-                    <option value="0" />
-                    <option value="25" />
-                    <option value="50" />
-                    <option value="75" />
-                    <option value="100" />
-                </datalist>
-            </div>
+            <If state={main}>
+                <div className={classes.options}>
+                    <label className={classes.checkbox}>
+                        <input type="checkbox"  />
+                        <span className={classes.checkboxSwitch}></span>
+                        Запуск игры во весь экран
+                    </label>
+                    <label className={classes.checkbox}>
+                        <input type="checkbox"  />
+                        <span className={classes.checkboxSwitch}></span>
+                        Запуск игры в дебаг режиме
+                    </label>
+                    <label className={classes.checkbox}>
+                        <input type="checkbox"  />
+                        <span className={classes.checkboxSwitch}></span>
+                        Автоматический вход на сервер
+                    </label>
+                    <label ref={RAMvalue}>Выделено оперативной памяти: 0</label><br />
+                    <input className={classes.slider} type="range" list="values" defaultValue="0" onChange={e => Slader(e)}/>
+                    <datalist id="values">
+                        <option value="0" label="5MG"/>
+                        <option value="25" label="10MG"/>
+                        <option value="50" label="20MG"/>
+                        <option value="75" label="30MG"/>
+                        <option value="100" label="40MG"/>
+                    </datalist>
+                </div>
+            </If>
+            <If state={info}>
+                <div>
+                <img src="./runtime/assets/images/logo.png" alt="" />
+                </div>
+            </If>
         </div>
     );
 }
