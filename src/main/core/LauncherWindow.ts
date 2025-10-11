bd61142c3ccd8569ba99eca9197878ebbcff094a
Andrew Molchanov <mr.catmanrus@gmail.com>
2025-10-11 08:56:42 +0300

    upd

--- src/main/core/LauncherWindow.ts
+++ src/main/core/LauncherWindow.ts
@@ -152,8 +152,8 @@ export class LauncherWindow {
         });
 
         // loading renderer code (runtime)
-        if (isDev && process.env['ELECTRON_RENDERER_URL'])
-            mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL']);
+        if (isDev && process.env.ELECTRON_RENDERER_URL)
+            mainWindow.loadURL(process.env.ELECTRON_RENDERER_URL);
         else mainWindow.loadFile(join(__dirname, '../renderer/index.html'));
 
         mainWindow.on('closed', () => {
